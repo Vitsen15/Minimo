c87e0f5a3a0e3833dd1e7efb81a9468a54a3c13a
A.Vitsentovich <vitsentovich.utd@gmail.com>
2017-08-01 10:41:46 +0300

    responsive markup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
-(function ($) {
-    
-})(jQuery);
+(function () {
+    var navigation = document.querySelector('.header__nav-list');
+    var header = document.querySelector('.header');
+    var width = window.innerWidth;
+
+    if (width <= 767) { //hide navigation when script starts
+        navigation.classList.remove('header__nav-list');
+        navigation.style.display = 'none';
+    }
+
+    window.addEventListener('resize', function () {
+        var width = window.innerWidth;
+
+        if (width <= 767) {
+            navigation.classList.remove('header__nav-list');
+            navigation.style.display = 'none';
+        } else {
+            navigation.style.display = 'inline-block';
+            navigation.classList.remove('header__nav-list--dropdown');
+            navigation.classList.add('header__nav-list');
+        }
+    });
+
+    var inc = 0;
+
+    window.showMenu = function () {
+
+        if (inc % 2 === 0) {
+            navigation.style.display = 'block';
+            navigation.classList.add('header__nav-list--dropdown');
+            header.style.paddingBottom = '0';
+            inc++;
+        } else {
+            navigation.classList.remove('header__nav-list--dropdown');
+            navigation.style.display = 'none';
+            header.style.paddingBottom = '15px';
+            inc++;
+        }
+    };
+})();
